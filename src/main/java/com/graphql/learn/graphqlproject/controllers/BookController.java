070863be0a64396851d6fb4b47e98d438111792e
Divya Gupta Arora <dgupta2@expediagroup.com>
2023-03-27 19:02:19 +0530

    mapping changed

--- src/main/java/com/graphql/learn/graphqlproject/controllers/BookController.java
+++ src/main/java/com/graphql/learn/graphqlproject/controllers/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/books/api")
 public class BookController {
 
     @Autowired
